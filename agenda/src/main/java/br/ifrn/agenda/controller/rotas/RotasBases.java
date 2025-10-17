a5821598e9b04bcd2ce1ba7151a5c28a215b8e60
J0a0p3dr067 <pjpedro158@gmail.com>
2025-10-17 18:57:11 -0300

    fix: nova porta 8181 para minha aplicação

--- agenda/src/main/java/br/ifrn/agenda/controller/rotas/RotasBases.java
+++
@@ -1,9 +0,0 @@
-package br.ifrn.agenda.controller.rotas;
-
-public interface RotasBases {
-    String FORM = "/form";
-    String LISTA = "/list";
-    String EDIT = "/edit";
-    String DELETE = "/delete";
-    String STRING = "/view";
-}
